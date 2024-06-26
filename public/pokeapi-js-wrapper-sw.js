e839f00af1b0c54d520bc19ad6da95961fe01cdf
Rafael Abensur <rafael@abensur.me>
2024-06-26 18:30:48 -0300

    fix: add sw.js

--- public/pokeapi-js-wrapper-sw.js
+++ public/pokeapi-js-wrapper-sw.js
@@ -1,2 +1,1 @@
-console.log('working')
 const imgRe = /https:\/\/raw\.githubusercontent\.com\/PokeAPI\/sprites\/[\/-\w\d]+\/[\d\w-]+\.(?:png|svg|gif)/; const version = 1; self.addEventListener('fetch', (e) => { e.request.url.match(imgRe) && e.respondWith(caches.match(e.request).then(((t) => { return t || fetch(e.request).then(((t) => { return e.request.url.match(imgRe) && caches.open('pokeapi-js-wrapper-images-1').then(((t) => { t.add(e.request.url) })), t })).catch(((e) => { console.error(e) })) }))) }), self.addEventListener('install', (e) => { self.skipWaiting() })
